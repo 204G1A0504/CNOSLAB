0aec807169bc29a4c483ada7e35aea06c1832a59
204G1A0504 <204G1A0504@srit.ac.in>
2022-11-01 13:10:09 +0530

    My FIFO.c program is completed successfully

--- OSLAB/FIFO.c
+++ OSLAB/FIFO.c
@@ -6,23 +6,26 @@
 */
 #include<stdio.h>
 int fr[3];
-void main()
+int main()
 {
 void display();
 int i,j,page[12]={2,3,2,1,5,2,4,5,3,2,5,2};
-int
-flag1=0,flag2=0,pf=0,frsize=3,top=0;
+int flag1=0,flag2=0,pf=0,frsize=3,top=0;
 for(i=0;i<3;i++)
 {
 fr[i]=-1;
 }
 for(j=0;j<12;j++)
 {
-flag1=0; flag2=0; for(i=0;i<12;i++)
+flag1=0; 
+flag2=0; 
+for(i=0;i<12;i++)
 {
 if(fr[i]==page[j])
 {
-flag1=1; flag2=1; break;
+flag1=1; 
+flag2=1; 
+break;
 }
 }
 if(flag1==0)
@@ -31,7 +34,9 @@ for(i=0;i<frsize;i++)
 {
 if(fr[i]==-1)
 {
-fr[i]=page[j]; flag2=1; break;
+fr[i]=page[j]; 
+flag2=1; 
+break;
 }
 }
 }
@@ -46,12 +51,11 @@ top=0;
 display();
 }
 printf("Number of page faults : %d ",pf+frsize);
-return 0;
 }
 void display()
 {
-int i; printf("\n");
-
+int i;
+printf("\n");
 for(i=0;i<3;i++)
 printf("%d\t",fr[i]);
 }
